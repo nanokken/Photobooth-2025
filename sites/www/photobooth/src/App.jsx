159c7945bcc02280b9d210e4a5797306c89e0c6c
nanokken <asge2055@mcdmstudent.dk>
2025-12-02 12:54:03 +0100

    Update App.jsx

--- sites/www/photobooth/src/App.jsx
+++ sites/www/photobooth/src/App.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import BrowserRouter as Router, { Route, Routes } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import Home from './pages/Home';
-import PhotoCarousel from './pages/PhotoCarousel';
+import Home from "./pages/Home";
+import PhotoCarousel from "./pages/PhotoCarousel";
 
 export default function App() {
   return (
