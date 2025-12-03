d509c6f0006e8a72b87b83e69920e8ee318a392c
nanokken <asge2055@mcdmstudent.dk>
2025-12-03 10:26:15 +0100

    last fix to main before starting

---
+++ photobooth/src/pages/AdminEvent/AdminEvent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './AdminEvent.module.css';
+
+export default function AdminEvent() {
+
+return(
+<div className={styles.container}>
+    <h1>Admin Event Page</h1>
+</div>
+)
+
+}
